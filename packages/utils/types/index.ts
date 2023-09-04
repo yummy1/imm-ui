23ce5928a08b8cea987b3a27f679be1f0efb8592
yummymao <1431608414@qq.com>
2023-09-04 09:34:56 +0800

    feat: 修改打包报错

--- packages/utils/types/index.ts
+++ packages/utils/types/index.ts
@@ -7,6 +7,9 @@ export {
     isPromise,
     isSymbol,
 } from '@vue/shared'
-export { isBoolean, isNumber } from '@vueuse/core'
+
+export const isBoolean = (o: any) => Object.prototype.toString.call(o).slice(8, -1) === 'Boolean'
+
+export const isNumber = (o: any) => Object.prototype.toString.call(o).slice(8, -1) === 'Number'
 
 export const isUndefined = (val: any): val is undefined => val === undefined
